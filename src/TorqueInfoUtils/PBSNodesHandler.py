e6078ccb10910e2ca40db52b1df2ceafe84ccf38
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2012-11-12 16:06:44 +0100

    pbsnodes parsing is fulfilled

--- src/TorqueInfoUtils/PBSNodesHandler.py
+++ src/TorqueInfoUtils/PBSNodesHandler.py
@@ -14,4 +14,134 @@
 # See the License for the specific language governing permissions and 
 # limitations under the License.
 
+import sys
+import re
+import time
+import shlex
+import subprocess
+from threading import Thread
 
+
+class ErrorHandler(Thread):
+
+    def __init__(self, err_stream):
+        Thread.__init__(self)
+        self.stream = err_stream
+        self.message = ""
+    
+    def run(self):
+        line = self.stream.readline()
+        while line:
+            self.message = self.message + line
+            line = self.stream.readline()
+
+
+
+class PBSNodesHandler(Thread):
+
+    def __init__(self, stream):
+        Thread.__init__(self)
+        self.stream = stream
+        self.errList = list()
+        self.container = list()
+        self.pRegex = re.compile('^\s*([^=\s]+)\s*=([^$]+)$')
+        
+        
+    def run(self):
+        line = self.stream.readline()
+        currTable = None
+        
+        while line:
+            parsed = self.pRegex.match(line)
+            if parsed and currTable <> None:
+            
+                key = parsed.group(1)
+                value = parsed.group(2).strip()
+                
+                if key == 'state':
+                
+                    currTable['state'] = value
+                    currTable['up'] = value <> 'down' and value <> 'offline'
+                
+                elif key == 'np':
+                    
+                    try:
+                        currTable['ncpu'] = int(value)
+                    except:
+                        currTable['ncpu'] = 0
+                
+                elif key == 'jobs':
+                
+                    if len(value) > 0:
+                        currTable['njob'] = value.count(',') + 1
+                    else:
+                        currTable['njob'] = 0
+                                        
+            else:
+            
+                nodeName = line.strip()
+                if len(nodeName) > 0:
+                    
+                    if currTable <> None:                            
+                        self.container.append(currTable)
+                        
+                    currTable = dict()
+                    currTable['name'] = nodeName
+                    currTable['up'] = False
+            
+            
+            line = self.stream.readline()
+
+        if currTable <> None:                            
+            self.container.append(currTable)
+        
+        #end of thread
+
+def parse(nodeList=[""], filename=None):
+
+    ncpu = 0
+    njob = 0
+    
+    try:
+    
+        for nodeId in nodeList:
+            if filename:
+                cmd = shlex.split('cat ' + filename)
+            else:
+                cmd = shlex.split('pbsnodes -a ' + nodeId)
+            process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        
+            stdout_thread = PBSNodesHandler(process.stdout)
+            stderr_thread = ErrorHandler(process.stderr)
+        
+            stdout_thread.start()
+            stderr_thread.start()
+        
+            ret_code = process.wait()
+        
+            stdout_thread.join()
+            stderr_thread.join()
+            
+            if ret_code <> 0:
+                raise Exception(stderr_thread.message)
+                
+            if len(stdout_thread.errList) > 0:
+                raise Exception(stdout_thread.errList[0])
+            
+            for nodeTable in stdout_thread.container:
+                if nodeTable['up']:
+                    ncpu += nodeTable['ncpu']
+                    njob += nodeTable['njob']
+            
+        return (ncpu, max(ncpu - njob, 0))
+
+    except:
+        etype, evalue, etraceback = sys.exc_info()
+        raise Exception("%s: (%s)" % (etype, evalue))
+
+
+if __name__ == "__main__":
+    
+    print parse([''], sys.argv[1])
+    
+    
