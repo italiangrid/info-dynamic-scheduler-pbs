f019c71fe6094484df52517b57a6450002f04727
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2016-11-28 13:47:23 +0100

    Minor changes for NVidia

--- src/TorqueInfoUtils/__init__.py
+++ src/TorqueInfoUtils/__init__.py
@@ -14,6 +14,10 @@
 # See the License for the specific language governing permissions and 
 # limitations under the License.
 
-__all__ = ["QStatHandler", "PBSNodesHandler", "MAUIHandler", "CommonUtils"]
+__all__ = [ "QStatHandler", 
+            "PBSNodesHandler",
+            "MAUIHandler",
+            "NvidiaSMIHandler",
+            "CommonUtils"]
 
 
